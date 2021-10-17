c4791a484f5ca130ff294293f30fca702ebb153d
ComputerCrunch <92650991+ComputerCrunch@users.noreply.github.com>
2021-10-17 13:02:02 +1300

    Update installcar.sh

--- installcar.sh
+++ installcar.sh
@@ -4,6 +4,16 @@ sudo apt update
 sudo apt-get full-upgrade
 sudo apt install matchbox-keyboard
 sudo apt install luakit
+git clone https://github.com/FD-/RPiPlay.git
+cd RPiPlay
+sudo apt-get install cmake
+sudo apt-get install libavahi-compat-libdnssd-dev
+sudo apt-get install libplist-dev
+sudo apt-get install libssl-dev
+mkdir build
+cd build
+cmake ..
+make -j
 echo Rebooting in 5
 sleep 1s
 echo 4
